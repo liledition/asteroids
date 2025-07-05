b8bc85211e70ab288a3a93abf370f12208097bb6
liledition <liledition@gmail.com>
2025-07-05 14:07:15 +0200

    playey start

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()  # Properly quit Pygame
+                pygame.quit()
                 return
+            
+        player.update(dt)
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
